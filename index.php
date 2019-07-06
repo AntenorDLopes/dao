db3bee3fbed96983cbe526c5398587b50c1b7296
Antenor Lopes Vieira <antenorlopes.d@gmail.com>
2019-07-06 01:56:21 -0500

    Metodos de Lista

--- index.php
+++ index.php
@@ -3,16 +3,42 @@
 	require_once("config.php");
 
 	/*$sql = new Sql();
-
 	$usuarios = $sql->select("SELECT * FROM tb_usuarios");
+	echo json_encode($usuarios);
 
-	echo json_encode($usuarios);*/
+	========================================================
+	FAZER UPDATE NO GIT:
+	git add --all
+	git commit -m "Criamos a Classe Usuario"
+	git push origin master
+	========================================================
 
+	*/
 
+	/*==============================
+		//CARREGA UM USUARIO//
 	$usuario = new Usuario();
 	$usuario->loadById(5);
 	echo "$usuario";
+	===============================*/
 
+	/*==============================
+	//CARREGA UMA LISTA USUARIO//
+	$lista = Usuario::getList();
+
+	echo json_encode($lista);
+	===============================*/
+
+	/*==============================
+	//CARREGA UMA LISTA USUARIO PELO LOGIN//
+	$search = Usuario::search("fe");
+	echo json_encode($search);
+	===============================*/
+
+	//CARREGA UM USUARIO USANDO UM LOGIN E SENHA
+	$usuario = new Usuario();
+	$usuario->login("fer","doe");
+	echo $usuario;
 
 
  ?>
